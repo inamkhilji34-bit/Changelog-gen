8105d25bc6cc3bfe83b1530563ea0f672af89b87
Inam Khilji <inamkhilji34@gmail.com>
2026-05-01 11:08:05 +0500

    debug: check env vars

--- web/main.py
+++ web/main.py
@@ -18,6 +18,13 @@ from core.changelog_generator import generate_changelog
 from web.database import save_changelog, get_changelog
 
 app = FastAPI()
+@app.get("/debug")
+async def debug():
+    import os
+    return {
+        "GROQ_API_KEY": os.getenv("GROQ_API_KEY", "NOT FOUND"),
+        "GITHUB_TOKEN": os.getenv("GITHUB_TOKEN", "NOT FOUND")
+    }
 templates = Jinja2Templates(directory=str(BASE_DIR / "web" / "templates"))
 
 @app.get("/", response_class=HTMLResponse)
